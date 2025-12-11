002fd7acabc3e01863db07dff5f848d6e74d2974
janokular <janekokularczyk@gmail.com>
2025-12-11 14:04:05 +0100

    updated routes.py

--- 926/routes.py
+++ 926/routes.py
@@ -1,10 +1,13 @@
 from flask import Blueprint
 from flask import render_template
 
+from .albums import get_albums
+
 
 main = Blueprint('routes', __name__)
 
 
 @main.route('/')
 def index_page():
-    return render_template('index.html')
+    albums = get_albums()
+    return render_template('index.html', albums=albums)
